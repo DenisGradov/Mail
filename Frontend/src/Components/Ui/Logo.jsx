da39b4dd9fd9e21f981d1e0b1f8b778e0a7695e1
Stenford <varonapika@gmail.com>
2025-04-18 02:13:44 +0200

    Change logo for test

--- Frontend/src/Components/Ui/Logo.jsx
+++ Frontend/src/Components/Ui/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <div className="flex justify-center items-center select-none">
       <SmallLogo/>
-      <h2 className="ml-[13px]  text-prim text-[29px] font-bold">Sten-mail</h2>
+      <h2 className="ml-[13px]  text-prim text-[29px] font-bold">Stenmail</h2>
 
     </div>
   );
